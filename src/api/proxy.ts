b1065606527f72ccccb45bbb516ad81cd414a025
Matt Kane <m@mk.gg>
2023-02-09 15:16:56 +0000

    Update

--- src/api/proxy.ts
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch(`https://internal.gatsbyjs.com`)
-  const data = await response.text()
-  res.status(200).json({ data })
-}
