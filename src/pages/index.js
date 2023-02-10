a530e9626efef7c902a69b2f78685c76da1a8868
Matt Kane <m@mk.gg>
2023-02-10 14:16:11 +0000

    Make empty site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,21 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import * as sections from "../components/sections"
-import Fallback from "../components/fallback"
-import SEOHead from "../components/head"
 
 export default function Homepage(props) {
-  const { homepage } = props.data
-
   return (
     <Layout>
-      {homepage.blocks.map((block) => {
-        const { id, blocktype, ...componentProps } = block
-        const Component = sections[blocktype] || Fallback
-        return <Component key={id} {...componentProps} />
-      })}
+      <Flex variant="column">
+        <Heading variant="mega" className={styles.heading}>
+          404
+        </Heading>
+        <Heading as="h1">Page not found</Heading>
+        <Flex variant="column" gap={0}>
+          <Text variant="lead" className={styles.text}>
+            Hello
+          </Text>
+        </Flex>
+      </Flex>
     </Layout>
   )
 }
-export const Head = (props) => {
-  const { homepage } = props.data
-  return <SEOHead {...homepage} />
-}
-export const query = graphql`
-  {
-    homepage {
-      id
-      title
-      description
-      image {
-        id
-        url
-      }
-      blocks: content {
-        id
-        blocktype
-        ...HomepageHeroContent
-        ...HomepageFeatureListContent
-        ...HomepageCtaContent
-        ...HomepageLogoListContent
-        ...HomepageTestimonialListContent
-        ...HomepageBenefitListContent
-        ...HomepageStatListContent
-        ...HomepageProductListContent
-      }
-    }
-  }
-`
